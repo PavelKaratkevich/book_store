64e84382289c890fdfba6e17658072332bf48ad5
PavelKaratkevich <p_korotkevitch@mail.ru>
2021-12-31 14:33:32 +0300

    make injection package and create func StartApp() in it

--- cmd/myapp.go
+++ cmd/myapp.go
@@ -1,30 +1,16 @@
 package main
 
 import (
-	"book_store/internal/handler"
-	"book_store/internal/service"
-	postgresdb "book_store/internal/repositoryDB/postgresDB"
+	"book_store/internal/injection"
 	"log"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Create DB client for PostgreSQL
-	db := postgresdb.ConnectDB()
-
-	// Wiring
-	postgresBookRepositoryDB := postgresdb.NewBookRepositoryDb(db)
-	bookService := service.NewBookService(postgresBookRepositoryDB)
-	bh := handler.BookHandler{Service: bookService}
-
-	// Creating router and defining routes and handlers
-	g := gin.Default()
-	g.GET("/books/", bh.GetAllBook)
-	g.GET("/books/:id/", bh.GetBookbyIdNumber)
+	// Start application
+	router := injection.StartApp()
 
 	// Running the connection on a defined port
-	if err := g.Run(":8080"); err != nil {
+	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
 }
