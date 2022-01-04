0704de0fe1cf47f13b19c49dda412317b6b86357
PavelKaratkevich <p_korotkevitch@mail.ru>
2022-01-04 18:48:08 +0300

    do minor changes with handlers functions

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -29,34 +29,38 @@ func (bh BookHandler) GetAllBook(ctx *gin.Context) {
 // GetBookbyIdNumber returns JSON with a particular book depending on its ID
 func (bh BookHandler) GetBookbyId(ctx *gin.Context) {
 
-	id, _ := strconv.Atoi(ctx.Param("id"))
-
-	res, err := bh.Service.GetBookById(id)
+	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(err.Code, gin.H{"error": err.Message})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unknown error"})
+		return
+	}
+
+	res, err2 := bh.Service.GetBookById(id)
+	if err2 != nil {
+		ctx.JSON(err2.Code, gin.H{"error": err2.Message})
 		return
 	}
 
 	ctx.JSON(http.StatusOK, res)
 }
 
-// UploadNewBook requires JSON fields (Title, Authors, Year) to be filled in, and sends back the number of books added 
+// UploadNewBook requires JSON fields (Title, Authors, Year) to be filled in, and sends back the number of books added
 func (bh BookHandler) UploadNewBook(ctx *gin.Context) {
 
 	var newBook domain.Book
-	
+
 	// Validating if all the fields are filled in
 	if err := ctx.ShouldBindJSON(&newBook); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "All fields should be filled in"} )
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "All fields should be filled in"})
 		return
 	}
-	
+
 	res, err := bh.Service.PostNewBook(newBook)
 	if err != nil {
 		ctx.JSON(err.Code, gin.H{"error": err.Message})
 		return
 	}
-	
+
 	ctx.JSON(http.StatusOK, res)
 }
 
@@ -64,18 +68,18 @@ func (bh BookHandler) UploadNewBook(ctx *gin.Context) {
 func (bh BookHandler) DeleteBook(ctx *gin.Context) {
 
 	id, _ := strconv.Atoi(ctx.Param("id"))
-	
+
 	rowsDeleted, err := bh.Service.DeleteBookById(id)
 	if err != nil {
-		ctx.JSON(err.Code, gin.H{"error": gin.H{"error":err.Message}})
+		ctx.JSON(err.Code, gin.H{"error": gin.H{"error": err.Message}})
 		return
 	}
-	
+
 	switch rowsDeleted {
 	case 0:
-		ctx.JSON(http.StatusNotFound, gin.H{"error":"ID not found"})
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "ID not found"})
 	case 1:
-		ctx.JSON(http.StatusOK, gin.H{"Message":"Book has been deleted successfully"})
+		ctx.JSON(http.StatusOK, gin.H{"Message": "Book has been deleted successfully"})
 	}
 }
 
@@ -84,25 +88,26 @@ and sends back status code and status message */
 func (bh BookHandler) UpdateBook(ctx *gin.Context) {
 	// Retrieving ID from URL
 	id, _ := strconv.Atoi(ctx.Param("id"))
-	
+
 	// Creating object which will be used for decoding fields of JSON request body (Title, Authors, Year)
-	updateBookRequest := domain.Book{ID: id,}
-	
+	updateBookRequest := domain.Book{ID: id}
+
 	// Validating if all the fields were filled in
 	if err := ctx.ShouldBindJSON(&updateBookRequest); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "All fields should be filled in"} )
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "All fields should be filled in"})
 		return
 	}
 	// Invoking service function
 	res, err := bh.Service.UpdateBookById(updateBookRequest)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error":"Internal error"})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal error"})
+		return
 	}
 	// Switching over RowsAffected: if 1 - status 200, if 0 - status 404
 	switch res {
 	case 0:
 		ctx.JSON(http.StatusNotFound, gin.H{"error": "ID not found"})
 	case 1:
-		ctx.JSON(http.StatusOK, gin.H{"Message":"Book has been updated successfully"})
+		ctx.JSON(http.StatusOK, gin.H{"Message": "Book has been updated successfully"})
 	}
 }
